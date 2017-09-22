34b267a2f8b64b0e275024a6ee1bbcfe436309a3
NeoLePorte <kagatookuda@gmail.com>
2017-09-22 17:40:50 -0400

    Add project files.

---
+++ MVCI Compendium(dockerless)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCI_Compendium_dockerless_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCI Compendium(dockerless)/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCI_Compendium_dockerless_.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
